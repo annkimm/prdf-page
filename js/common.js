f5792ad73a79659d27c3b40af762d6ed7a6f92cf
annakim <zizicoo208@naver.com>
2018-08-30 03:24:38 +0900

    html css 5차 완료
    js는 아직 2차 진행 중

--- js/common.js
+++ js/common.js
@@ -1,16 +1,84 @@
-var header = document.getElementById("header");
-var header_height = header.offsetHeight;
-var addClass = header.classList.add("on");
-var removeClass = header.classList.remove("on");
 
-function scrollHeader() {
-  if (header_height < $(window).scrollTop()) {
-    addClass;
-  } else {
-    removeClass;
-  }
+window.addEventListener('load',onloadH);
+
+function onloadH(e){
+	var bodyHeight= window.innerHeight;
+	var main=document.getElementsByClassName("section__main")
+	main[0].style.height=bodyHeight+"px";
+	resizeH();
 }
 
-scrollHeader();
+window.addEventListener("resize",resizeH)
 
-$(document).ready(function() {});
+function resizeH (e){
+	var bodyHeight= window.innerHeight;
+	var main=document.getElementsByClassName("section__main")
+	main[0].style.height=bodyHeight+"px";
+
+}
+	
+window.addEventListener("scroll",function(){
+
+	var header = document.getElementById("header__height");
+	var header_height = header.offsetHeight	
+	
+	if (header_height < window.pageYOffset) {
+		header.classList.add("on");
+	} else {
+		header.classList.remove("on");
+	}
+	
+	var windowScroll=$(window).scrollTop();
+	var $section=$(".section__div")
+
+	$.each($section,function(idx,item){
+
+		var sectionIdx=$section.eq(idx);
+		var menuScroll=sectionIdx.offset().top-$("#header__height").height()-2;
+		
+		if( menuScroll <= windowScroll ){
+			
+			$("#header__menu li").eq(idx).addClass("on").siblings("li").removeClass("on");
+			$("#header__menu li").eq(0).removeClass("on")
+         }
+
+		
+	});
+	
+});	
+
+$(document).ready(function() {
+	
+	var menu=$("#header__menu li>a")
+	
+	$.each(menu,function(idx,evt){
+		$(this).click(function(evt){
+			
+			evt.preventDefault();
+			
+			var header_height = document.getElementById("header__height").offsetHeight;
+			
+			var idxplus=idx+1
+			var menu_parent=$(this).parent("li");
+			
+			if(idx==0){
+				menu_parent.siblings("li").removeClass("on");
+			}else{
+						
+				menu_parent.addClass("on").siblings("li").removeClass("on");	
+			}
+			
+			$('html, body').animate({
+				scrollTop: $("#section"+idxplus).offset().top-header_height
+			}, 1000);
+		
+		});
+		
+		
+	});
+
+});
+
+
+
+
